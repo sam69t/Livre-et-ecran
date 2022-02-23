08572bb97c42520d1e1918fa0ccf26a6afae2fd6
sam69t <samueldumezz@yahoo.fr>
2022-02-23 12:38:10 +0100

    tt

--- preload.js
+++ preload.js
@@ -1,14 +1,38 @@
 let queue = new createjs.LoadQueue(false);
 let images = new Array(2585);
 
+const container = document.querySelector(".container");
+
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
 queue.on("progress", (event) => {
   let progress = Math.floor(event.progress * 100);
-  console.log(progress);
+  let progressMin = Math.floor(progress / 10);
+
+  // console.log(scale(progress, 0, 100, 0, 1));
+
+  let num = scale(progress, 0, 100, 0, 1);
+
+  console.log(num);
+  container.style.opacity = num;
+  document.getElementById("expl").style.opacity = num;
 
   document.getElementById("loader").textContent = progress + "%";
 
   if (progress == 100) {
-    console.log("all done");
+    document.getElementById("loader").textContent = "Entrer";
+    $("#loader").addClass("enter");
+
+    $(".enter").click(function () {
+      $(".home").css({
+        "-webkit-transform": "translateX(-100%)",
+        "-ms-transform": "translateX(-100%)",
+        transform: "translateX(-100%)",
+      });
+
+      console.log("click");
+    });
   }
 });
 
