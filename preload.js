8b35d361c8d77e41d4375ef8dfd059e320861524
sam69t <samueldumezz@yahoo.fr>
2022-02-23 19:40:55 +0100

    JustTo1000

--- preload.js
+++ preload.js
@@ -2,6 +2,7 @@ let queue = new createjs.LoadQueue(false);
 let images = new Array(2585);
 
 const container = document.querySelector(".container");
+var el = document.getElementById("preloadjs-container");
 
 function scale(number, inMin, inMax, outMin, outMax) {
   return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
@@ -14,7 +15,7 @@ queue.on("progress", (event) => {
 
   let num = scale(progress, 0, 100, 0, 1);
 
-  console.log(num);
+  // console.log(num);
   container.style.opacity = num;
   document.getElementById("expl").style.opacity = num;
 
@@ -25,26 +26,30 @@ queue.on("progress", (event) => {
     $("#loader").addClass("enter");
 
     $(".enter").click(function () {
-      $(".home").css({
-        "-webkit-transform": "translateX(-100%)",
-        "-ms-transform": "translateX(-100%)",
-        transform: "translateX(-100%)",
-      });
-
+      $(".home").css("opacity", "0");
       console.log("click");
+      setTimeout(() => {
+        $(".home").css("display", "none");
+      }, 600);
     });
   }
 });
 
-queue.on("fileload", handleFileComplete);
-function handleFileComplete(event) {}
-
 function preLoad() {
-  for (var i = 0; i < 2585; i++) {
+  for (var i = 0; i < 1000; i++) {
     // images[i] = new Image();
     // images[i].src = "assets/" + i + ".jpg";
     queue.loadFile("assets/" + i + ".jpg");
+    console.log("loading");
   }
 }
+// function preLoadTwo() {
+//   for (var i = 1000; i < 2000; i++) {
+//     // images[i] = new Image();
+//     // images[i].src = "assets/" + i + ".jpg";
+//     queue.loadFile("assets/" + i + ".jpg");
+//     console.log("loading");
+//   }
+// }
 
 preLoad();
